20ff0a02c040471b846161847dc12203c581c882
José Augusto <jose.11881444@aluno.mg.gov.br>
2026-09-10 16:30:08 -0300

    feat: muda a palavra de devolver pra alugar, mas somente uma vez

--- alugames-main/js/app.js
+++ alugames-main/js/app.js
@@ -1,8 +1,9 @@
 const tirarTelaApagada = document.getElementById("game-3-img");
 const mudarBotao = document.getElementById("button-3");
-
+const textoBotao = document.getElementById("mudarTextoNoBotão-3");
 function alterarStatus() {
     tirarTelaApagada.classList.toggle("dashboard__item__img--rented"); 
      mudarBotao.classList.toggle("dashboard__item__button--return"); 
-    
+     textoBotao.innerText = "Alugar";
+     textoBotao.classList.toggle("mudarTextoNoBotão-3")
     }
