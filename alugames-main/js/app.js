777574db35260e0039f0d5164771caa6347eea65
Mateus Filipe Souza Martins <mateus.11881423@aluno.mg.gov.br>
2026-09-14 14:19:47 -0300

    feat: sistema completo de trocar nome, cor do botão e de escurecer a imagem em todos os jogos

--- alugames-main/js/app.js
+++ alugames-main/js/app.js
@@ -1,16 +1,13 @@
-const tirarTelaApagada = document.getElementById("game-3-img");
-const mudarBotao = document.getElementById("button-3");
-const textoBotao = document.getElementById("mudarTextoNoBotão-3");
+function alterarStatus(id) {
+    const tirarTelaApagada = document.getElementById(`game-${id}-img`);
+    const mudarBotao = document.getElementById(`button-${id}`);
+    const textoBotao = document.getElementById(`mudarTextoNoBotão-${id}`);
 
-
-
-function alterarStatus() {
-    tirarTelaApagada.classList.toggle("dashboard__item__img--rented"); 
-     mudarBotao.classList.toggle("dashboard__item__button--return"); 
-     textoBotao.classList.toggle("mudarTextoNoBotão-3")
-     if (tirarTelaApagada.classList.contains("dashboard__item__img--rented")){
+    tirarTelaApagada.classList.toggle("dashboard__item__img--rented");
+    mudarBotao.classList.toggle("dashboard__item__button--return");
+    textoBotao.classList.toggle("mudarTextoNoBotão-${id}");
+    if (tirarTelaApagada.classList.contains("dashboard__item__img--rented")) {
         textoBotao.innerText = "Devolver";
     }else{textoBotao.innerText = "Alugar";}
-    
-     
     }
+ 
