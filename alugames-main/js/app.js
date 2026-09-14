66f1e82f48e586894d40e2a27c41f2b555758f49
Mateus Filipe Souza Martins <mateus.11881423@aluno.mg.gov.br>
2026-09-14 13:50:54 -0300

    feat: fizemos funcionar a função base do botão.

--- alugames-main/js/app.js
+++ alugames-main/js/app.js
@@ -1,9 +1,16 @@
 const tirarTelaApagada = document.getElementById("game-3-img");
 const mudarBotao = document.getElementById("button-3");
 const textoBotao = document.getElementById("mudarTextoNoBotão-3");
+
+
+
 function alterarStatus() {
     tirarTelaApagada.classList.toggle("dashboard__item__img--rented"); 
      mudarBotao.classList.toggle("dashboard__item__button--return"); 
-     textoBotao.innerText = "Alugar";
      textoBotao.classList.toggle("mudarTextoNoBotão-3")
+     if (tirarTelaApagada.classList.contains("dashboard__item__img--rented")){
+        textoBotao.innerText = "Devolver";
+    }else{textoBotao.innerText = "Alugar";}
+    
+     
     }
